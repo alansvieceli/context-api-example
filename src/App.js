3edeba3db421cd5ada0fcf583c27ea4b39f7fff7
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-08-23 15:56:59 -0300

    Testezinho com Context API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
-function App() {
+const ThemeContext = React.createContext('light');
+
+function ThemedButton(props) {
   return (
-    <div>      
-        <h2>Teste</h2>
-    </div>
+    <ThemeContext.Consumer>
+      {theme => (
+        <a style={{ backgroundColor: theme === 'dark' ? '#333' : '#CCC' }}>
+          Meu botão
+        </a>
+      )}
+    </ThemeContext.Consumer>
   );
+}
+
+class App extends React.Component {
+  state = {
+    theme: 'light',
+  };
+
+  changeTheme = () => {
+    this.setState({ 
+      theme: this.state.theme === 'light' 
+        ? 'dark' 
+        : 'light'
+    });
+  }
+
+  render() {
+    return (
+      <ThemeContext.Provider value={this.state.theme}>
+        <button onClick={this.changeTheme}>Alterar tema</button>
+        <ThemedButton />
+      </ThemeContext.Provider>
+    );
+  }
+
 }
 
 export default App;
